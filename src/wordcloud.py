f3af5f8cc7b823e0f1192a06de0e10c05902bc58
Jia-Yi Jian <jyjian2@illinois.edu>
2020-03-01 16:56:47 -0600

    update words in sourcefile and add font size

--- src/wordcloud.py
+++ src/wordcloud.py
@@ -2,12 +2,19 @@
 import sys
 import string
 from PIL import Image, ImageDraw, ImageFont
+import random
 
 class WordCloud:
 
     def __init__(self, filename):
         self.filename = filename
         self.sorted_word_tuples = []
+        self.read_stop_words()
+
+    def read_stop_words(self):
+        file = open("src/stop_words.txt", "r")
+        self.stop_words = file.read().split('\n')
+        file.close()
 
     def load(self):
         string.punctuation
@@ -18,7 +25,9 @@ class WordCloud:
         for w in file_str.split():
             for p in string.punctuation:
                 w = w.replace(p, '')
-            if w not in wrd_freq_dict:
+            if w in self.stop_words:
+                continue
+            elif w not in wrd_freq_dict:
                 wrd_freq_dict[w] = 1
             else:
                 wrd_freq_dict[w] += 1
@@ -30,8 +39,15 @@ class WordCloud:
         self.image_path = 'test.png'
         img = Image.new('RGB', (600, 600), color = 'white')
         d = ImageDraw.Draw(img)
-        fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf', size=50)
-        d.text((300,300), 'Helloxxx', font=fnt, fill=(148,0,211))
+        sum_freq = 0
+        for text in self.sorted_word_tuples:
+            sum_freq += int(text[1])
+        for text in self.sorted_word_tuples:
+            x = random.randrange(50,550)
+            y = random.randrange(50,550)
+            sz = int((text[1] / sum_freq) * 3600)
+            fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf', sz)
+            d.text((x,y), text[0], font=fnt, fill=(148,0,211))
         img.save(self.image_path)
 
 
--- test/test-wordcloud.py
+++ test/test-wordcloud.py
@@ -6,13 +6,20 @@ class TestWordCloud(unittest.TestCase):
         wc = WordCloud('test/test-paragraph01.txt')
         wc.load()
         self.assertEqual(wc.sorted_word_tuples[0], ('pig', 4))
-        self.assertEqual(wc.sorted_word_tuples[1], ('a', 3))
+        self.assertEqual(wc.sorted_word_tuples[1], ('apple', 3))
+
+    def test_read_stop_words(self):
+        wc = WordCloud('test/test-paragraph00.txt')
+        self.assertEqual(len(wc.stop_words), 204)
+        self.assertEqual(wc.stop_words[0], 'a')
 
     def test_toImage(self):
         wc = WordCloud('test/test-paragraph00.txt')
         wc.load()
         wc.toImage()
         self.assertTrue(os.path.exists(wc.image_path))
+
+
 
 if __name__ == '__main__':
     unittest.main()
