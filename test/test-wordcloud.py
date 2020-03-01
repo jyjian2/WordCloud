16ffe9dccf501fe2e9c0433955c7ee8621bb3f4e
Jia-Yi Jian <jyjian2@illinois.edu>
2020-03-01 11:19:04 -0600

    update test case

--- test/test-wordcloud.py
+++ test/test-wordcloud.py
@@ -9,7 +9,7 @@ class TestWordCloud(unittest.TestCase):
         self.assertEqual(wc.sorted_word_tuples[1], ('a', 3))
 
     def test_toImage(self):
-        wc = WordCloud('test/test-paragraph01.txt')
+        wc = WordCloud('test/test-paragraph00.txt')
         wc.load()
         wc.toImage()
         self.assertTrue(os.path.exists(wc.image_path))
